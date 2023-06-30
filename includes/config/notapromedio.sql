656641e0d4236673a940152c2a09cb69ba280ab6
Cristian Javier Rivera <javi-cali1995@hotmail.com>
2023-06-30 10:01:37 -0500

    Version 2

--- includes/config/notapromedio.sql
+++ includes/config/notapromedio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-06-2023 a las 22:57:34
+-- Tiempo de generación: 30-06-2023 a las 17:00:29
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
